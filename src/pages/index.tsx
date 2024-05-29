235d8c718677e0097d431d93493f3cfcf9ac29d8
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2024-05-29 11:37:35 +0900

    test ready

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,10 @@
+import { AngleDown } from "flowbite-react-icons/outline";
+
 export default function Home() {
-  return <p>main page</p>;
+  return (
+    <p>
+      main page
+      <AngleDown />
+    </p>
+  );
 }
